4bd548d91a96ad0310b71d4456c5cbff4aff49f8
Dominic Vicente <dvicente4482@sdsu.edu>
2026-02-28 23:29:40 -0800

    Add clickable marker

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,41 @@
 import { View } from 'react-native';
 import AboutScreen from './aboutScreen';
 import ImageC from './image';
+import PinDetails from './pinDetails';
 
 export default function Index() {
   return (
     <>
-    <View style={{ flex: 1, 
-        justifyContent: "center",
-        alignItems: "center",
-    }}>
-    </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />
 
-    <ImageC
-     source={{ uri: 'https://ontheworldmap.com/usa/city/san-diego/sdsu-campus-map.jpg' }}
-     style={{ width: "90%",
-       height: "90%",
-       justifyContent: "flex-start",
-       position: "relative",
-       alignItems: "center",
-       margin:"auto" }}
-    />
-    <ImageC 
-    style={{position: "absolute", top: 200, left: 200, width: "10%", height: "10%"}} source={require("../assets/images/marker.png")}
-    />
-    <AboutScreen />
+      <ImageC
+        source={{ uri: 'https://ontheworldmap.com/usa/city/san-diego/sdsu-campus-map.jpg' }}
+        style={{
+          width: "90%",
+          height: "90%",
+          justifyContent: "flex-start",
+          position: "relative",
+          alignItems: "center",
+          margin: "auto",
+        }}
+      />
 
+      {/* Marker */}
+      <PinDetails
+        source={require("../assets/images/marker.png")}
+        style={{
+          position: "absolute",
+          top: 200,
+          left: 200,
+          width: 50,
+          height: 50,
+          zIndex: 999,
+        }}
+      />
 
-    <View style={{ backgroundColor: 'lightblue', width: 2000, height: 100 }}>
-    </View>
+      <AboutScreen />
 
-    
-    
-    
+      <View style={{ backgroundColor: 'lightblue', width: 2000, height: 100 }} />
     </>
-
   );
 }
